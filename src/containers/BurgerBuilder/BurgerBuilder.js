58bc84240d5077e0e61517e24b2ddbbbaf7ca4e1
Carlos <carlos.delgado@hotmail.it>
2020-06-04 10:21:13 +0200

    Adding the burger builder class component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class BurgerBuilder extends Components {
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </Aux>
+    )
+  }
+}
+
+export default BurgerBuilder
