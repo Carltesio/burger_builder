1f305209c323f4674eac71607654240be4fb59f2
Carlos <carlos.delgado@hotmail.it>
2020-06-04 10:11:59 +0200

    Wrapping the layout component with the new aux and exporting

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Aux from '../../hoc/Aux'
 
 const layout = () => (
-    
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
-        {props.children}
-    </main>
+
+    <Aux>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
 )
+
+export default layout;
