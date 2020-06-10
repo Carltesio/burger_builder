4821e656298e50de7c357cb038a49817d88997d2
Carlos <carlos.delgado@hotmail.it>
2020-06-10 09:53:50 +0200

    Adding a message if the array of ingredientisempty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,17 +5,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
 
-  const transformedIngredient = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
   .map(ingrKey => {
     return [...Array(props.ingredients[ingrKey])].map((_, i) => {
     return  <BurgerIngredient key = {ingrKey + i} type={ingrKey} />
     });
-  })
-
+  }).reduce((arr, el) => {
+    return arr.concat(el)
+  }, []);
+if (transformedIngredients.length === 0) {
+transformedIngredients = <p>Please start adding ingredients!</p>
+}
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      {transformedIngredient}
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
