fca70d94383b86b662979de7d5656ee11a6ff29e
Carlos <carlos.delgado@hotmail.it>
2020-06-11 10:51:49 +0200

    adding disble button in case of fals eof a given type of contrlo

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,21 +4,23 @@ import classes from './BuildControls.module.css';
 import BuildControl from './BuildControl/BuildControl'
 
 const controls = [
-    { label: 'Salad', type: 'salad' },
-    { label: 'Bacon', type: 'bacon' },
-    { label: 'Cheese', type: 'cheese' },
-    { label: 'Meat', type: 'meat' }
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
 ];
 
 const buildControls = (props) => (
-    <div className={classes.BuildControls}>
-{controls.map(crtl => (
-    <BuildControl
-         key={crtl.label} 
-         label={crtl.label}
-         added={ () => props.ingredientAdded(crtl.type)}/>
-))}
-    </div>
+  <div className={classes.BuildControls}>
+    {controls.map(crtl => (
+      <BuildControl
+        key={crtl.label}
+        label={crtl.label}
+        added={() => props.ingredientAdded(crtl.type)}
+        removed={() => props.ingredientRemoved(crtl.type)}
+        disabled={props.disabled[crtl.type]} />
+    ))}
+  </div>
 );
 
 export default buildControls;
