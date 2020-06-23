bc71802fe8113519e55b74e6f05236c5e927eefa
Carlos <carlos.delgado@hotmail.it>
2020-06-23 15:18:31 +0200

    Adding Button to order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button'
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
@@ -20,8 +21,8 @@ const orderSummary = (props) => {
         {ingredientSummary}
       </ul>
       <p>Continue to checkout</p>
-      <button>CANCEL</button>
-      <button>CONTINUE</button>
+      <Button>CANCEL</Button>
+      <Button>CONTINUE</Button>
     </Aux>
   )
 }
