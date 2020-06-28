d7e5f384627cda4aaaacd85aff8ec600f4b908b2
Carlos <carlos.delgado@hotmail.it>
2020-06-28 13:45:00 +0200

    Adding logo component and image

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import burgerLogo from '../../assets/images/original.png'
+
+const logo = (props) => (
+<div>
+    <img src={burgerLogo} alt="Myburger" />
+</div>
+);
+
+export default logo;
