2f7f8cf6ae3f7d8847f48946434d89e4d08867d7
Carlos <carlos.delgado@hotmail.it>
2020-06-28 13:48:23 +0200

    Adding logo style

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+import classes from './Logo.module.css';
 import burgerLogo from '../../assets/images/original.png'
 
 const logo = (props) => (
-<div>
+<div className={classes.Logo}>
     <img src={burgerLogo} alt="Myburger" />
 </div>
 );
