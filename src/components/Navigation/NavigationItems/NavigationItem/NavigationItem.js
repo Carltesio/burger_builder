f36eb6874b7d6452420c31db026a0ca5ff9c06e5
Carlos <carlos.delgado@hotmail.it>
2020-07-01 14:00:53 +0200

    passing the navcomponent to the toolbar to render it on the page

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import React from 'react';
 
 import classes from '../NavigationItem/NavigationItem.module.css';
 
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import classes from '../NavigationItems/NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
-  <ul className={classes.navigationItems}>
+  <ul className={classes.NavigationItems}>
     <NavigationItem link="/" active>Burger Builder</NavigationItem>
     <NavigationItem link="/">Checkout</NavigationItem>
   </ul>
