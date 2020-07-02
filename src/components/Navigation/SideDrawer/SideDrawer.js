a117b794129591d8388778bf0bc046564037d01d
Carlos <carlos.delgado@hotmail.it>
2020-07-02 17:36:30 +0200

    changing the positionond style dinamyc

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,7 +7,7 @@ import classes from './SideDrawer.module.css';
 const sideDrawer = (props) => {
   return (
     <div className={classes.SideDrawer}>
-      <div>
+      <div className={classes.Logo}>
       <Logo />
       </div>
       <nav>
