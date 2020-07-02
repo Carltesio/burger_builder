a512822cd158c77aed6b5387974b8ddd102fe713
Carlos <carlos.delgado@hotmail.it>
2020-07-02 17:31:12 +0200

    Add style for logo dinamically

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,7 +7,9 @@ import classes from './SideDrawer.module.css';
 const sideDrawer = (props) => {
   return (
     <div className={classes.SideDrawer}>
+      <div>
       <Logo />
+      </div>
       <nav>
         <NavigationItems />
       </nav>
